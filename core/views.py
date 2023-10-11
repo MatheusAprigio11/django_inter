eeb5942f08cd68869e23cd538b407a98614e67cd
MatheusAprigio11 <aprigiomatheus24@gmail.com>
2023-10-11 17:00:06 -0300

    initial commit

---
+++ core/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+from .forms import ContatoForm
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def contato(request):
+    form = ContatoForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'contato.html', context)
+
+
+def produto(request):
+    return render(request, 'produto.html')
