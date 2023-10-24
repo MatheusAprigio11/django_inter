df2ccc3ae601659d6b74bf92fbd0cc125730ef5d
matheusaprigio11 <aprigiomatheus24@gmail.com>
2023-10-24 16:59:20 -0300

    updt

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from stdimage.models import StdImageField
 
-# Create your models here.
+from django.db.models import signals
+from django.template.defaultfilters import slugify
+
+class Base(models.Model):
+    criado = models.DateField('Data de Criação', auto_now_add=True)
+    modificado = models.DateField('Data de Atualização', auto_now=True)
+    ativo = models.BooleanField('Ativo?', default=True)
+
+    class Meta:
+        abstract = True
+
+class Produto(Base):
+    nome = models.CharField('nome', max_length=100)
+    preco = models.DecimalField('Preço', max_digits=8, decimal_places=2)
