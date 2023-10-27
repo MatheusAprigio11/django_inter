6e7bdc3f3c1d79b420b127c5e9c45382f19aa742
MatheusAprigio11 <aprigiomatheus24@gmail.com>
2023-10-27 11:27:04 -0300

    updt

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Produto
 
-# Register your models here.
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'preco', 'estoque', 'slug', 'criacao', 'modificado', 'ativo')
--- core/models.py
+++ core/models.py
@@ -15,3 +15,14 @@ class Base(models.Model):
 class Produto(Base):
     nome = models.CharField('nome', max_length=100)
     preco = models.DecimalField('Preço', max_digits=8, decimal_places=2)
+    estoque = models.IntegerField('Estoque')
+    imagem = StdImageField('Imagem', upload_to='produtos', variations={'thumb':(124,124)})
+    slug = models.SlugField('Slug', max_length=100, blank=True, editable=False)
+
+    def __str__(self):
+        return self.nome
+    
+def produto_pre_save(signal, instance, sender, **kwargs):
+    instance.slug = slugify(instance.nome)
+
+signals.pre_save.connect(produto_pre_save, sender=Produto)
